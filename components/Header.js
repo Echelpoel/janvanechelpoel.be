fcb38d182f8f10e1d0e19a565ebe14b6694ab2e6
Jan Van Echelpoel <janvanechelpoel@gmail.com>
2019-11-10 10:58:45 +0100

    Remove hire banner

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import MaxWidthContainer from 'components/MaxWidthContainer'
 import SocialLinkList from 'components/SocialLinkList'
-import HireBanner from 'components/HireBanner'
 import { SITE_NAME } from 'constants/global'
 import {
     HEADING,
@@ -20,10 +19,9 @@ const Header = () => (
                 <SocialLinkList />
             </nav>
             <div className="contentContainer">
-                <img src="/static/img/me-lol-2.png" alt="le moi" />
+                <img src="/static/img/me-lol.png" alt="le moi" />
                 <h1>{HEADING}</h1>
                 <p className="subHeading">{SUBHEADING}</p>
-                <HireBanner />
                 <div className="CTAContainer">
                     <div className="CTALabel">{CTA_LABEL}</div>
                     <a href={CTA_URL}>{CTA_TEXT}</a>
